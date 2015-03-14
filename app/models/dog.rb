fe3fd8b1954feddcb6b7b35b3ae5bdda115fca63
Mikaelo Espiritu <mikaelo_0812@yahoo.com>
2015-03-13 19:13:31 -0700

    I'M DONE

---
+++ app/models/dog.rb
@@ -0,0 +1,7 @@
+class Dog < ActiveRecord::Base
+
+  # The User model.  Our abstraction to access the database.
+
+  validates :name, presence: true # Validates that there is a name
+  validates :age, presence:true # Validates that there is an age
+end
